e3e1503f3e6f07c49fe268b66f4221c71733e913
Daniel Cujba <daniel.cujba.54@gmail.com>
2024-11-13 23:54:47 +0200

    Fmm razvane

--- WebApi/Context/JyrosContext.cs
+++ WebApi/Context/JyrosContext.cs
@@ -24,10 +24,6 @@ public partial class JyrosContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=jyros.database.windows.net;Database=Jyros;User Id=jyros-admin;Password=Gev$sgaNKPFG9Z4;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Sprint>(entity =>

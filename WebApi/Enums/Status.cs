92084fd2197eefe27b14e4944b726226f015099e
Daniel Cujba <daniel.cujba.54@gmail.com>
2024-11-14 17:38:54 +0200

    fuck my life
    Fuck git, all my hommies hate git
    
    AI coxat done

---
+++ WebApi/Enums/Status.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace WebApi.Enums
+{
+    public enum Status
+    {
+        [Description("open")]
+        Open,
+        [Description("in progress")]
+        InProgress,
+        [Description("completed")]
+        Completed
+    }
+}
